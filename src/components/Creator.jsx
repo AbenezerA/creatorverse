a335ce1248ece55ac78c7a143fe6b79b84a6dc71
AbenezerA <ata2152@columbia.edu>
2023-08-19 17:38:29 +0300

    setup poject

---
+++ src/components/Creator.jsx
@@ -0,0 +1,11 @@
+import { React, useEffect, useState } from 'react'
+
+const Creator = () => {
+    return(
+        <div class="Creator">
+            <h1>Hello Creator</h1>
+        </div>
+    )
+}
+
+export default Creator;
