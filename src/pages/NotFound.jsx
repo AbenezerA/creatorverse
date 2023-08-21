449f97a697767ebe2c2723759895d02ba83666b6
AbenezerA <ata2152@columbia.edu>
2023-08-21 12:19:59 +0300

    implemented not-found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,10 @@
+import { React, useState, useEffect } from 'react';
+import { Outlet, Link, useParams } from "react-router-dom";
+
 const NotFound = () => {
     return(
-        <div>
-            <h1>Not Found</h1>
+        <div className="not-found">
+            <h2>Oops, this page does not exist! Click <Link to="/">here</Link> to go back to the landing page.</h2>           
         </div>
     )
 }
